dd27cf80945c7dde60aeaa8edf31fadc78289975
divverma2003 <divyamalikverma@gmail.com>
2025-10-26 12:22:44 -0400

    setup for mongodb connection

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("User signed up");
+};
+
+export const login = async (req, res) => {
+  res.send("User logged in");
+};
+
+export const logout = async (req, res) => {
+  res.send("User logged out");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import express from "express";
-
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./lib/connectMongoDB.js";
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
 });
